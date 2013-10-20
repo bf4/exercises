a3010dd4f8fea206489b1f28ab94ff49a95b03cb
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-20 15:48:10 -0500

    First test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 require 'rspec'
 require 'rspec/autorun'
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'tic_tac_toe'
+
+describe TicTacToe do
+  example do
+    TicTacToe
+  end
+end
