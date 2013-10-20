70ec05da75108a9acbd94a5a277f3a74876ff8c3
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-20 15:59:54 -0500

    Add basic board and space

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,20 @@
 class TicTacToe
   class Board
+    Space = Struct.new(:column, :row) do
+
+    end
+    WIDTH  = 3
+    HEIGHT = 3
+    attr_reader :spaces
+
+    def initialize
+      @spaces = []
+      1.upto(HEIGHT) do |row|
+        1.upto(WIDTH) do |column|
+          spaces << Space.new(column, row)
+        end
+      end
+    end
 
   end
 
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -16,4 +16,28 @@ describe TicTacToe do
     expect(game.board).to be_a(TicTacToe::Board)
   end
 
+  describe TicTacToe::Board do
+
+    let(:board) { TicTacToe::Board.new }
+
+    example do
+      board
+    end
+
+    it 'has 9 spaces' do
+      expect(board.spaces.size).to eq(9)
+    end
+
+    describe TicTacToe::Board::Space do
+      let(:column) { 1 }
+      let(:row)    { 3 }
+      let(:space)  { TicTacToe::Board::Space.new(column, row) }
+      example do
+        expect(space.column).to eq(column)
+        expect(space.row).to    eq(row)
+      end
+    end
+  end
+
+
 end
