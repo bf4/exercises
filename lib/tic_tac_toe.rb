c3174c00645c78e253af4df9224c8cf888acbf12
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-20 15:51:48 -0500

    Add Board

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,13 @@
 class TicTacToe
+  class Board
 
+  end
+
+  def start
+    true
+  end
+
+  def board
+    Board.new
+  end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,4 +5,15 @@ describe TicTacToe do
   example do
     TicTacToe
   end
+
+  let(:game) { TicTacToe.new }
+
+  it "can start a game" do
+    expect(game.start).to be_true
+  end
+
+  it "has a board" do
+    expect(game.board).to be_a(TicTacToe::Board)
+  end
+
 end
