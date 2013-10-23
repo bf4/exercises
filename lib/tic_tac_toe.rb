9b4174e582eed8a1678f84171fe39d8de83fe1aa
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-22 22:24:26 -0500

    Introduce notion of "winnable sets"

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,13 +3,36 @@ class TicTacToe
     Space = Struct.new(:column, :row) do
 
       def empty?
+        value == :empty
+      end
+
+      def set(value)
+        return false unless empty?
+        @value = value
         true
+      end
+
+      def value
+        @value ||= :empty
       end
 
     end
     WIDTH  = 3
     HEIGHT = 3
-    attr_reader :spaces
+    SpaceSet = Struct.new(:spaces) do
+      WINNING = 2
+      def winning_move?(move)
+        values.count(move) == WINNING
+      end
+      def winning_move(move)
+        winning_move?(move) && spaces.find{|space|space.empty?}
+      end
+      private
+      def values
+        spaces.reject(&:empty?).map(&:value)
+      end
+    end
+    attr_reader :spaces, :winnable_sets
 
     def initialize
       @spaces = []
@@ -17,6 +40,20 @@ class TicTacToe
         1.upto(WIDTH) do |column|
           spaces << Space.new(column, row)
         end
+      end
+      @winnable_sets = [
+        [ [1,1], [1,2], [1,3] ],
+        [ [2,1], [2,2], [2,3] ],
+        [ [3,1], [3,2], [3,3] ],
+        [ [1,1], [2,1], [3,1] ],
+        [ [1,2], [2,2], [3,2] ],
+        [ [1,3], [2,3], [3,3] ],
+        [ [1,1], [2,2], [3,3] ],
+        [ [1,3], [2,2], [3,1] ]
+      ].map do |set|
+        SpaceSet.new Array(set.map do |coords|
+          space(row: coords[0], column: coords[1])
+        end)
       end
     end
 
@@ -28,6 +65,13 @@ class TicTacToe
 
     def empty?
       spaces.all?(&:empty?)
+    end
+
+    def winning_move(player)
+      winnable_sets.find do |winnable_set|
+        winning_move = winnable_set.winning_move(player)
+        break winning_move if winning_move
+      end
     end
 
   end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -36,15 +36,66 @@ describe TicTacToe do
       expect(space.column).to eq(column)
     end
 
+    it 'the board has a set of winning space set combinations' do
+      winnable_sets = board.winnable_sets
+      expect(winnable_sets.size).to eq(8)
+      expect(winnable_sets.first.spaces.first).to eq(board.space(row: 1, column: 1))
+    end
+
+    it 'the board can return a winning space set' do
+      board.space(row: 1, column: 1).set('x')
+      board.space(row: 3, column: 3).set('x')
+      expect(board.winning_move('x')).to eq(board.space(row: 2, column: 2))
+    end
+
     describe TicTacToe::Board::Space do
       let(:column) { 1 }
       let(:row)    { 3 }
       let(:space)  { TicTacToe::Board::Space.new(column, row) }
       example do
-        expect(space.column).to eq(column)
-        expect(space.row).to    eq(row)
+        expect(space.column).to   eq(column)
+        expect(space.row).to      eq(row)
+        expect(space.value).to    eq(:empty)
+      end
+      it 'can act on an empty space' do
+        expect(space).to be_empty
+        space.set('x')
+        expect(space.value).to eq('x')
+      end
+      it 'can act on an empty space' do
+        space.set('x')
+        expect(space.set('y')).to be_false
       end
     end
+
+    describe TicTacToe::Board::SpaceSet do
+      let(:space_set) do
+        TicTacToe::Board::SpaceSet.new(
+          [[1,1], [1,2], [1,3]].map do |column_row|
+          TicTacToe::Board::Space.new(*column_row)
+        end)
+      end
+      it 'contains an array of spaces' do
+        expect(space_set.spaces.size).to eq(3)
+      end
+      it 'tells if there is a winning move' do
+        space_set.spaces[0..1].each do |space|
+          space.set('x')
+        end
+        expect(space_set.winning_move?('x')).to be_true
+        expect(space_set.winning_move?('o')).to be_false
+      end
+      it 'returns the winning space' do
+        space_set.spaces[0..1].each do |space|
+          space.set('x')
+        end
+        winning_move = space_set.winning_move('x')
+        expect(winning_move.row).to eq(3)
+        expect(winning_move.column).to eq(1)
+      end
+
+    end
+
   end
 
   describe TicTacToe::Strategy do
